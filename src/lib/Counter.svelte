4a5e729d092093ac7eefdd67ae03c4c4904a1547
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2021-10-13 23:05:59 -0300

    Criada estrutura inicial da página;

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,18 @@
+<script>
+  let count = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button class="counter-btn" on:click={increment}>
+  Clicks: {count}
+</button>
+
+<style>
+  .counter-btn {
+    @apply btn;
+    @apply bg-yellow-700;
+    @apply border-yellow-800;
+  }
+</style>
