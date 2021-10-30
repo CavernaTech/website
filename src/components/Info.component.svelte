e89a26855fa3b1f8a9b5390cde4fdaff353da035
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2021-10-30 18:53:32 -0300

    Implementado esboço do footer;

---
+++ src/components/Info.component.svelte
@@ -0,0 +1,29 @@
+<script>
+  export var href = "";
+  export var icon = "";
+  export var title = "";
+</script>
+
+<main>
+  <a class="info" href={href}>
+    <div class="ico">
+      <img src={icon} alt="icone">
+    </div>
+    <div class="title">
+      <h3>{title}</h3>
+    </div>
+  </a>
+</main>
+
+<style lang="postcss" type="text/postcss">
+  .info {
+    @apply flex flex-row;
+  }
+  .ico {
+    @apply w-1/5 h-10;
+  }
+  .title {
+    @apply w-4/5 h-10;
+  }
+
+</style>
--- src/pages/main/Footer.svelte
+++ src/pages/main/Footer.svelte
@@ -1,16 +1,45 @@
 <script>
+import Info from "../../components/Info.component.svelte";
+
 </script>
 
 <main>
-    <div class="footer">
-
+  <div class="footer">
+    <div class="info">
+      <h3>Entre em contato conosco:</h3>
+      <Info href="https://wa.me/5504797402365" title="WhatsApp - J. Gabriel G." />
+      <Info href="https://wa.me/5504792037866" title="WhatsApp - Josnei D." />
+      <Info href="mailto:jgabrielgruber@hotmail.com" title="E-mail - J. Gabriel G." />
+      <Info href="mailto:josneidrosdek@gmail.com" title="WhatsApp - Josnei D." />
+      <Info href="https://www.instagram.com/cavernatech.dev" title="Instagram - CavernaTech" />
     </div>
+    <form action="#" class="form">
+      <h3>Deixe seu contato conosco:</h3>
+      <input id="name" type="text" placeholder="Nome" />
+      <input id="email" type="email" placeholder="E-mail" />
+      <input id="tel" type="tel" placeholder="Telefone" />
+      <select name="subject" id="subject">
+        <option value="app">Aplicativo</option>
+        <option value="platform">Plataforma</option>
+        <option value="website">Site</option>
+        <option value="portal">Portal</option>
+        <option value="iot">Automação</option>
+      </select>
+      <button type="submit">Enviar</button>
+    </form>
+  </div>
 </main>
 
 <style lang="postcss" type="text/postcss">
-    .footer {
-        @apply flex items-center justify-center min-h-screen;
-        @apply bg-gray-800;
-        @apply dark:bg-gray-200;
-    }
+  .footer {
+    @apply flex min-h-screen;
+    @apply bg-gray-800;
+    @apply dark:bg-gray-800;
+  }
+  .info {
+    @apply flex flex-col w-2/4 p-20;
+  }
+  .form {
+    @apply flex flex-col w-2/4 p-20;
+  }
 </style>
