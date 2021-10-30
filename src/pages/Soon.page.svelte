cde26210a57581737038d2429e55202e93a987ae
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2021-10-30 15:12:43 -0300

    Refatorado para página de embreve;

---
+++ src/pages/Soon.page.svelte
@@ -0,0 +1,37 @@
+<script>
+</script>
+
+<main>
+  <div class="flex items-center justify-center h-screen bg-gray-200">
+    <div class="container">
+      <div class="text-center">
+        <h2
+          class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
+        >
+          CavernaTech
+        </h2>
+        <h3 class="text-xl md:text-3xl mt-10">Em Breve</h3>
+        <p class="text-md md:text-xl mt-10">
+          <a class="hover:underline" href="https://www.cavernatech.dev"
+            >CavernaTech</a
+          > é uma empresa de desenvolvimento de softwares, aplicativos, plataformas,
+          IoT, portais, etc.
+        </p>
+      </div>
+      <div class="flex flex-wrap mt-10 justify-center">
+        <div class="m-3">
+          <a
+            href="https://www.instagram.com/cavernatech.dev/"
+            title="CavernaTech no Instagram"
+            class="md:w-32 bg-gray-200 tracking-wide text-gray-800 font-bold rounded border-2 border-pink-600 hover:border-pink-600 hover:bg-pink-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+          >
+            <span class="mx-auto">Instagram</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
