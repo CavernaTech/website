f2d464f21d677ff98798906918f1133935140fe4
Drosdek <josneidrosdek@gmail.com>
2022-05-10 15:51:31 -0300

    Criado component Tab;

---
+++ src/components/Tabs/Tab.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { getContext } from "svelte";
+	import { TABS } from "./Tabs.svelte";
+
+	export let icon = null;
+
+	const tab = {};
+	const { registerTab, selectTab, selectedTab } = getContext(TABS);
+
+	registerTab(tab);
+</script>
+
+<button
+	class:selected={$selectedTab === tab}
+	on:click={() => selectTab(tab)}
+	class="flex p-4"
+>
+	<svelte:component
+		this={icon}
+		class="flex-shrink h-6 w-6 text-purple-600 mr-4"
+	/>
+	<slot />
+</button>
+
+<style lang="postcss" type="text/postcss">
+	button {
+		@apply m-0;
+	}
+	.selected {
+		@apply border-b-2 border-purple-600 text-purple-600;
+	}
+</style>
---
+++ src/components/Tabs/TabList.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-row tab-list">
+	<slot></slot>
+</div>
+
+<style lang="postcss" type="text/postcss">
+	.tab-list {
+		@apply border-b-2 border-purple-600
+	}
+</style>
