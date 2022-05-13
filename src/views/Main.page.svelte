7b4861810d7362e63ff8077e3e7a3e4b3003c7b9
Drosdek <josneidrosdek@gmail.com>
2022-05-13 15:01:51 -0300

    Refatorado;

--- src/views/Main.page.svelte
+++ src/views/Main.page.svelte
@@ -1,43 +1,11 @@
 <script>
+  import Banner from "../components/Banner.svelte";
   import Contact from "../components/Contact.component.svelte";
   import IntroCavernaTech from "../components/Intro.component.svelte";
   import Solution from "../components/Solutions/Solution.component.svelte";
 </script>
 
 <main>
-  <div
-    class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen"
-  >
-    <div
-      class="absolute top-0 w-full h-full bg-center bg-cover"
-      style="
-        background-image: url(https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80);
-      "
-    >
-      <span
-        id="blackOverlay"
-        class="w-full h-full absolute opacity-75 bg-black"
-      />
-    </div>
-    <div class="container relative mx-auto">
-      <div class="items-center flex flex-wrap">
-        <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-          <div class="pr-12">
-            <h1 class="text-white font-semibold text-5xl">
-              Nao deixe sua empresa para tras!
-            </h1>
-            <p class="mt-4 text-lg text-white">
-              A capacidade de inovar é determinante para a competitividade das
-              empresas!<br />
-              Sabendo disso a CavernaTech possui a mais diversa stack de desenvolvimento
-              para atender as mais diversas necessidades do mercado, atendemos desde
-              de solucoes web, aplicativos, sistemas ate automacao industrial!
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
   <IntroCavernaTech />
   <Solution />
   <Contact />
