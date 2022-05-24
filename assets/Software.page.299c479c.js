458050086ae8d2fd8e1b1d7cb4739bf206b020de
JGabrielGruber <JGabrielGruber@users.noreply.github.com>
2022-05-24 18:39:24 +0000

    deploy: 3aefb8190ac9be26cddab60b7970c354f2461a7c

---
+++ assets/Software.page.299c479c.js
@@ -0,0 +1,41 @@
+import{S as F,i as N,s as V,m as $,f as t,e as o,a as n,o as j,q as T,b as C,g as a,n as z,v as q,w as M,x as S,d as H}from"./vendor.064e269e.js";import{H as G,C as B,F as J}from"./index.8c4401f4.js";function K(A){let i,w,e,c,_,p,L,u,D,g,I,d,E,x,k,f,P,m,R,h,O,r,y,l,b;return i=new G({}),r=new B({}),l=new J({}),{c(){$(i.$$.fragment),w=t(),e=o("main"),c=o("div"),c.innerHTML=`<div class="hero-overlay bg-opacity-60"></div> 
+    <div class="hero-content text-center text-neutral-content"><div class="max-w-lg"><h1 class="mb-5 text-6xl font-bold">Software house com melhor em tecnologia de ponta a ponta!</h1> 
+        <p class="mb-5 text-2xl">A CavernaTech \xE9 uma software house especialista em desenvolver
+          sistemas e aplicativos sob medida. Com atua\xE7\xE3o flex\xEDvel, avaliamos
+          seus objetivos de neg\xF3cio para propor solu\xE7\xF5es de tecnologia bem
+          fundamentados</p></div></div>`,_=t(),p=o("div"),p.innerHTML=`<div class="hero-content flex-col lg:flex-row"><img src="assets/img/factory.jpg" alt="F\xE1brica pronta para produ\xE7\xE3o" class="min-w-xs lg:max-w-lg rounded-lg"/> 
+      <div><h1 class="text-6xl font-bold">In\xEDcio r\xE1pido e entregas frequentes</h1> 
+        <p class="py-6 text-2xl">Agilidade \xE9 parte da cultura CavernaTech. Com aloca\xE7\xE3o r\xE1pida de
+          profissionais, iniciamos projetos na velocidade que sua empresa
+          precisa e entregamos valor agregado continuamente.</p></div></div>`,L=t(),u=o("div"),u.innerHTML=`<div class="hero-content flex-col lg:flex-row-reverse"><img src="assets/img/structure.jpg" alt="Pessoas planejando projeto" class="min-w-xs lg:max-w-lg rounded-lg"/> 
+      <div><h1 class="text-6xl font-bold">Redu\xE7\xE3o de custo &amp; Time-to-market</h1> 
+        <p class="py-6 text-2xl">NElimine seu esfor\xE7o de contrata\xE7\xE3o e comece a desenvolver o seu
+          projeto imediatamente. Montar times internos consome muito tempo. Com
+          uma equipe CavernaTech voc\xEA lan\xE7a seus projetos de tecnologia mais
+          r\xE1pido com qualidade garantida.</p></div></div>`,D=t(),g=o("div"),g.innerHTML=`<div class="hero-content text-center"><div class="max-w-lg"><h1 class="text-6xl font-bold">Excel\xEAncia em engenharia de software</h1> 
+        <p class="py-6 text-2xl">Mais de 10 anos de experi\xEAncia em desenvolvimento de software,
+          sistemas de miss\xE3o cr\xEDtica e alta disponibilidade. Vasto conhecimento
+          em: Microservices, Serverless, Codeless, IaaS, TDD, BDD, Integration
+          Patterns, Arquitetura Evolutiva, Engenharia de Dados, Design System e
+          outras pr\xE1ticas.</p></div></div>`,I=t(),d=o("div"),d.innerHTML=`<div class="hero-overlay bg-opacity-60"></div> 
+    <div class="hero-content text-center text-neutral-content"><div class="max-w-lg"><h1 class="mb-5 text-6xl font-bold">Qualidade, seguran\xE7a e boas pr\xE1ticas</h1> 
+        <p class="mb-5 text-2xl">Testes autom\xE1ticos, unit\xE1rios e manuais atendem as metas de qualidade.
+          Efici\xEAncia de ponta a ponta com Ciclo de Vida Seguro de
+          Desenvolvimento de Software e certifica\xE7\xE3o PCI/DSS.</p></div></div>`,E=t(),x=o("div"),x.innerHTML=`<div class="hero-content flex-col lg:flex-row-reverse"><img src="assets/img/teamwork.jpg" alt="Pessoas comprometidas" class="min-w-xs lg:max-w-lg rounded-lg"/> 
+      <div><h1 class="text-6xl font-bold">Gest\xE3o eficiente e inovadora</h1> 
+        <p class="py-6 text-2xl">Gest\xE3o proativa nas necessidades de cada cliente com KPIs que
+          monitoram as entregas. Se voc\xEA precisa, a CavernaTech se adapta.
+          Olhamos pra cada indiv\xEDduo, mantendo o fit cultural, monitorando
+          engajamento e indicadores de performance.</p></div></div>`,k=t(),f=o("div"),f.innerHTML=`<div class="hero-content text-center"><div class="max-w-lg"><h1 class="text-6xl font-bold">Retorno sobre investimento (ROI)</h1> 
+        <p class="py-6 text-2xl">Velocidade para come\xE7ar, zero burocracia, alto conhecimento em
+          engenharia de software e redu\xE7\xE3o dos seus custos internos tornam as
+          equipes da CavernaTech a melhor op\xE7\xE3o para aumentar seu retorno sobre
+          investimento (ROI).</p></div></div>`,P=t(),m=o("div"),m.innerHTML=`<div class="hero-overlay bg-opacity-60"></div> 
+    <div class="hero-content text-center text-neutral-content"><div class="max-w-lg"><h1 class="mb-5 text-6xl font-bold">Responsabilidade com o resultado</h1> 
+        <p class="mb-5 text-2xl">Ningu\xE9m conhece o seu neg\xF3cio mais do que voc\xEA. Juntos defineremos
+          metas e crit\xE9rios de sucesso mensur\xE1veis. Os projetos ser\xE3o executados
+          visando atender a estes crit\xE9rios.</p></div></div>`,R=t(),h=o("div"),h.innerHTML=`<div class="hero-content flex-col lg:flex-row"><img src="assets/img/startup.jpg" alt="Equipe respons\xE1vel" class="min-w-xs lg:max-w-lg rounded-lg"/> 
+      <div><h1 class="text-6xl font-bold">Melhores talentos do mercado</h1> 
+        <p class="py-6 text-2xl">Nossos crivos de sele\xE7\xE3o garantem profissionais experientes, com uma
+          cultura que valoriza excel\xEAncia em engenharia de software e boas
+          pr\xE1ticas.</p></div></div>`,O=t(),$(r.$$.fragment),y=t(),$(l.$$.fragment),n(c,"class","hero min-h-screen bg-gray-400"),j(c,"background-image","url(assets/img/industry.jpg)"),n(p,"class","hero min-h-screen bg-base-200"),n(u,"class","hero min-h-screen bg-gray-800 text-gray-200"),n(g,"class","hero min-h-screen bg-base-200"),n(d,"class","hero min-h-screen"),j(d,"background-image","url(assets/img/analytics.jpg)"),n(x,"class","hero min-h-screen bg-gray-800 text-gray-200"),n(f,"class","hero min-h-screen bg-base-200"),n(m,"class","hero min-h-screen"),j(m,"background-image","url(assets/img/conference.jpg)"),n(h,"class","hero min-h-screen bg-base-200")},m(s,v){T(i,s,v),C(s,w,v),C(s,e,v),a(e,c),a(e,_),a(e,p),a(e,L),a(e,u),a(e,D),a(e,g),a(e,I),a(e,d),a(e,E),a(e,x),a(e,k),a(e,f),a(e,P),a(e,m),a(e,R),a(e,h),a(e,O),T(r,e,null),C(s,y,v),T(l,s,v),b=!0},p:z,i(s){b||(q(i.$$.fragment,s),q(r.$$.fragment,s),q(l.$$.fragment,s),b=!0)},o(s){M(i.$$.fragment,s),M(r.$$.fragment,s),M(l.$$.fragment,s),b=!1},d(s){S(i,s),s&&H(w),s&&H(e),S(r),s&&H(y),S(l,s)}}}class W extends F{constructor(i){super();N(this,i,null,K,V,{})}}export{W as default};
