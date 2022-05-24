3af15637df83c8cc4a64a30cd8bb08c64a084cdb
JGabrielGruber <JGabrielGruber@users.noreply.github.com>
2022-05-24 00:26:05 +0000

    deploy: 8ba8c10a304f8ee9e995a8a5acc8367cdc130a0a

---
+++ assets/Mobile.page.ca9a39f6.js
@@ -0,0 +1,22 @@
+import{S as C,i as D,s as F,m as b,f as o,e as l,a as m,o as L,q as h,b as $,g as a,n as N,v as y,w,x as _,d as j}from"./vendor.064e269e.js";import{H as U,C as k,F as z}from"./index.65ebbf97.js";function E(T){let t,x,s,p,q,r,H,v,M,g,S,d,A,i,f,n,u;return t=new U({}),i=new k({}),n=new z({}),{c(){b(t.$$.fragment),x=o(),s=l("main"),p=l("div"),p.innerHTML=`<div class="hero-content flex-col lg:flex-row"><img src="assets/img/metodologia-agil.webp" alt="Aplicativos robustos e complexos" class="max-w-xl rounded-lg"/> 
+      <div><h1 class="text-6xl font-bold">Desenvolvimento de Aplicativos</h1> 
+        <p class="py-6 text-2xl">Cr\xEDamos aplicativos conforme suas necessidades,
+          com enf\xE2se em aplicativos Flutter, PWAs em ReactJS,
+          e estando flex\xEDveis ao desenvolvimento nativo no mobile!</p></div></div>`,q=o(),r=l("div"),r.innerHTML=`<div class="hero-overlay bg-opacity-60"></div> 
+    <div class="hero-content text-center text-neutral-content"><div class="max-w-lg"><h1 class="mb-5 text-6xl font-bold">Desenvolvimento \xE1gil de aplicativos</h1> 
+        <p class="mb-5 text-2xl">Somos mobile expert, e experts em metodologia \xE1gil. Nossos
+          desenvolvedores fazem muito mais do que escrever c\xF3digos. Geramos
+          valor a partir das melhores tecnologias e linguagens.</p></div></div>`,H=o(),v=l("div"),v.innerHTML=`<div class="hero-content flex-col lg:flex-row-reverse"><img src="assets/img/structure.jpg" alt="" class="max-w-xl rounded-lg"/> 
+      <div><h1 class="text-6xl font-bold">Especifica\xE7\xE3o e Arquitetura de aplicativos</h1> 
+        <p class="py-6 text-2xl">Nossa preocupa\xE7\xE3o \xE9 garantir todo e qualquer processo, de forma
+          estruturada, escalavel e flex\xEDvel. E claro, garantindo boas pr\xE1ticas
+          em QA e DevOps, como manuten\xE7\xE3o e seguran\xE7a para o projeto do
+          aplicativo</p></div></div>`,M=o(),g=l("div"),g.innerHTML=`<div class="hero-content text-center"><div class="max-w-lg"><h1 class="text-6xl font-bold">Mobile UX Design fluida e consistente</h1> 
+        <p class="py-6 text-2xl">Solu\xE7\xF5es reais, coerentes e atraentes de UX e UI, combinadas com boas
+          pr\xE1ticas de design para Android ou iOS. Buscamos a melhor experi\xEAncia
+          para o seu cliente.</p></div></div>`,S=o(),d=l("div"),d.innerHTML=`<div class="hero-overlay bg-opacity-60"></div> 
+    <div class="hero-content text-center text-neutral-content"><div class="max-w-lg"><h1 class="mb-5 text-6xl font-bold">Gest\xE3o pr\xF3xima e indicadores</h1> 
+        <p class="mb-5 text-2xl">Nossa equipe de desenvolvimento mobile apoia seu projeto de app em
+          todas as etapas. Somos treinados a responder de forma \xE1gil e assertiva
+          a cada valida\xE7\xE3o do projeto, a fim de cumprir prazos e garantir
+          qualidade.</p></div></div>`,A=o(),b(i.$$.fragment),f=o(),b(n.$$.fragment),m(p,"class","hero min-h-screen bg-base-200"),m(r,"class","hero min-h-screen bg-gray-400"),L(r,"background-image","url(assets/img/business_mobile.jpg)"),m(v,"class","hero min-h-screen bg-gray-800 text-gray-200"),m(g,"class","hero min-h-screen bg-base-200"),m(d,"class","hero min-h-screen"),L(d,"background-image","url(assets/img/analytics.jpg)")},m(e,c){h(t,e,c),$(e,x,c),$(e,s,c),a(s,p),a(s,q),a(s,r),a(s,H),a(s,v),a(s,M),a(s,g),a(s,S),a(s,d),a(s,A),h(i,s,null),$(e,f,c),h(n,e,c),u=!0},p:N,i(e){u||(y(t.$$.fragment,e),y(i.$$.fragment,e),y(n.$$.fragment,e),u=!0)},o(e){w(t.$$.fragment,e),w(i.$$.fragment,e),w(n.$$.fragment,e),u=!1},d(e){_(t,e),e&&j(x),e&&j(s),_(i),e&&j(f),_(n,e)}}}class X extends C{constructor(t){super();D(this,t,null,E,F,{})}}export{X as default};
