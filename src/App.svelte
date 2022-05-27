069ca2bfbb250fcaa07483ccb264212e1facdbea
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2022-05-27 09:41:05 -0300

    Adicionado Termos de Cookies;

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,12 @@
   import Router from "svelte-spa-router";
   import { wrap } from "svelte-spa-router/wrap";
   import Index from "./views/index.page.svelte";
-
+  import TermsController from "./controllers/terms.controller";
+  import Terms from "./components/Terms.component.svelte";
+  
+  let agreed = TermsController.isAgreed();
+  let whenAgreed = TermsController.whenAgreed();
+  const handleAgreement = TermsController.agreeTerms;
   const routes = {
     "/": Index,
     "/mobile": wrap({
@@ -13,6 +18,32 @@
     }),
     "*": Index,
   };
+    (function (i, s, o, g, r, a, m) {
+      i["GoogleAnalyticsObject"] = r;
+      (i[r] =
+        i[r] ||
+        function () {
+          (i[r].q = i[r].q || []).push(arguments);
+        }),
+        (i[r].l = 1 * new Date());
+      (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m);
+    })(
+      window,
+      document,
+      "script",
+      "https://www.google-analytics.com/analytics.js",
+      "ga"
+    );
+    if (agreed) {
+      ga("create", "G-3DNY4MVGWD", "auto");
+      ga("send", "pageview");
+    }
 </script>
 
-<Router {routes} />
+<div>
+  <Terms bind:agreed onAgreement={handleAgreement} bind:whenAgreed banner />
+  <Router {routes} />
+</div>
